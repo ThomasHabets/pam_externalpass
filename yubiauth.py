646a7882eb086d7ccd6bb7e8bc4cb3ceb9c3b724
Thomas Habets <thomas@habets.pp.se>
2009-06-17 18:39:52 +0200

    yubiauth.py: logging added

--- yubiauth.py
+++ yubiauth.py
@@ -1,28 +1,21 @@
-#!/usr/bin/python
+#!/usr/local/bin/python
 
 import sys
 import os.path
 import pwd
 import os
 import re
+import logging
+import logging.handlers
+
+logger = logging.getLogger('yubiauth.py')
+#logger.setLevel(logging.DEBUG)
+handler = logging.handlers.SysLogHandler("/dev/log")
+handler.setFormatter(logging.Formatter("%(name)s[%(process)d] %(message)s"))
+logger.addHandler(handler)
 
 userconf_envname = "PAM_EXTERNALPASS_USERCONF"
 maxConfFileSize = 1000000 # max ~/.yubikeys file size, in bytes
-
-if False:
-    from M2Crypto import Rand, SSL, httpslib
-
-    def get_https(host, url, port=443, certs=('cacert.crt',)):
-        ctx = SSL.Context()
-        for cert in certs:
-            if ctx.load_verify_locations(cert) != 1:
-                raise Exception('CA certificates not loaded')
-        ctx.set_verify(SSL.verify_peer | SSL.verify_fail_if_no_peer_cert, 9)
-        h = httpslib.HTTPSConnection(host, port, ssl_context=ctx)
-        h.putrequest('GET', '/')
-        h.endheaders()
-        resp = h.getresponse()
-        return resp
 
 def dvorak2qwerty(s):
     dvorak = "`1234567890[]',.pyfgcrl/=aoeuidhtns-\\<;qjkxbmwvz"
@@ -41,15 +34,21 @@ class Authenticator:
         pass
     class ErrNotice(ErrBase):
         pass
+
     def verifyToken(self, token, url):
+        #logger.debug("Verifying token %s with URL %s" % (token, url))
         url = url % ({'token': token})
         # FIXME: log if we nedded to remove evil
         url = re.sub(r'[^A-Za-z0-9:;/.,%!@^&()+ ?=-]', '', url)
         try:
-            fo = os.popen("curl -s '%s'" % (url))
+            cmd = "curl -s '%s'" % (url)
+            #logger.debug("Running curl: <%s>" % (cmd))
+            fo = os.popen(cmd)
             rs = fo.read()
         except IOError, e:
+            logger.warning("popen() failed: " + str(e))
             raise self.ErrBadPassword()
+        logger.debug("Curl returned <%s>" % (rs))
 
         if rs == "OK":
             return True
@@ -59,7 +58,9 @@ class Authenticator:
 
     def getConfEntries(self, fn):
         d = open(fn).read(maxConfFileSize)
-        #FIXME: if (len(d) > x): log warning
+        if len(d) > maxConfFileSize/2:
+            logger.warning("User <%s> has a big (at least %d bytes)"
+                           % (user, len(d)))
         for line in d.split('\n'):
             a,b = line.split(None, 1)
             yield a,b
@@ -67,6 +68,7 @@ class Authenticator:
     def run(self):
         user = raw_input("")
         token = raw_input("")
+        #logger.debug("Checking token %s for user %s" % (token, user))
         if len(token) < 44:
             return "FAIL"
         keyid = token[-44:][:12]
@@ -76,25 +78,28 @@ class Authenticator:
         try:
             for key,url in self.getConfEntries(fn):
                 if key == keyid or key == dvorak2qwerty(keyid):
+                    logger.debug("Found key <%s>, trying it with url: <%s>"
+                                 % (key, url))
                     # FIXME: handle same key, different authserver
                     self.verifyToken(token, url)
                     return "OK"
             raise self.ErrBase("Yubikey not listed as authorized")
 
         except self.ErrUsername, e:
-            # FIXME: log
+            logger.debug("User does not have keyid <%s> in their conf"
+                         % (keyid))
             return "FAIL"
 
         except self.ErrNotice, e:
-            # FIXME: log
+            logger.debug("NOTICE sent to user %s" % (user))
             return str(e)
 
         except self.ErrBase, e:
-            # FIXME: log
+            logger.debug("Auth error of user %s: %s" % (user, e))
             return "FAIL"
 
         except:
-            # FIXME: log
+            logger.debug("Unknown error for user %s: %s" % (user, str(e)))
             return "FAIL"
 
 def main():
